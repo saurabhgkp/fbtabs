aed0ec083ba725b6e78469d387eccf5cbce57928
pcology <pcologyofficial@gmail.com>
2022-11-09 15:18:12 +0530

    working on login

--- controllers/users.js
+++ controllers/users.js
@@ -1,16 +1,25 @@
 const User = require("../models/usres");
+const bcrypt = require("bcrypt");
+
 
 exports.register = async (req, res) => {
   const { name, email, password } = req.body;
-  console.log(name, email);
-
+  console.log(name, email, password);
+  const hashpassword = await bcrypt.hash(password, 10);
   try {
     const userInDB = await User.find({ email: email });
     if (userInDB.length == 0) {
-      const newUser = new User({ name, email, password });
-      await newUser.save();
+      const data = new User({ name, email, hashpassword });
+      await data.save();
+      if (data) {
+        const token = jwt.sign({ userId: data.id }, process.env.JWT_SECRET, {
+          expiresIn: "10000h",
+        });
+
+      }
       res.status(201).json({
         message: "Register Successfully",
+        token: token,
       });
     } else {
       res.status(200).json({
@@ -36,15 +45,7 @@ exports.login = async (req, res) => {
   try {
     const isData = await bcrypt.compare(password, data.newPassword);
 
-    if (data && isData) {
-      const token = jwt.sign({ id: data.id }, "saurabh");
-      return res.status(200).json({
-        status: 1,
-        token: token,
-      });
-    } else {
-      res.send("user not found");
-    }
+
   } catch (err) {
     return res.status(500).json({
       status: 0,
