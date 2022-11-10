2739aba1e1c017c361a83369cf4c344b57669ed0
pcology <pcologyofficial@gmail.com>
2022-11-10 11:08:42 +0530

    login down

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var userController = require("../controllers/users");
-//const isAuth = require('../middleware/isAuth')
+const isAuth = require('../middleware/isAuth')
 
 router.get("/", function (req, res) {
   res.send("this is User Route");
@@ -11,9 +11,20 @@ router.post("/register", userController.register);
 
 router.get("/verify/:userId/:uniqueString", userController.verify);
 
+//Forgot password
 
 router.post("/login", userController.login);
 
+// Address Contact Number add 
 
+// Address Contact Number edit
+
+// Address Contact Number delete
+
+// checkout model  userId ProductId status address  Schedule time Instructions Subtotal(jsonb)
+
+//Delivery Schedule, Delivery Instructions,Delivery Tip, Subtotal
+
+//stripe payment 
 
 module.exports = router;
